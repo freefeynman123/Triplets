a4b64b2927ab019d607064befe05f1ef3445736e
Lukasz <lukasz3.bala@gmail.com>
2019-10-30 20:33:15 +0100

    Adding losses

--- triplets/classifier.py
+++
@@ -1,7 +0,0 @@
-from sklearn.model_selection import GridSearchCV
-from sklearn.neighbors import KNeighborsClassifier
-
-params = {'n_neighbors': list(range(3, 10))}
-classifier = KNeighborsClassifier()
-clf = GridSearchCV(classifier, params, cv=10)
-clf.fit(X_train_codes, y_train_codes)
